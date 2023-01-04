fd2681b8c1e0a22db6ee6074d5e90f2df37ef29d
Xavier Mendez <44298432+xavierlmendez@users.noreply.github.com>
2023-01-04 13:10:42 -0600

    added docker container

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 
 function App() {
   return (
-    <body className ="bg-mainImage bg-auto">
+    //bg-mainImage refers to mainImage specified in tailwind.config.js under backGround
+    //min-h-screen makes the vertical size of the background image fit the device screen
+    <body className ="bg-mainImage bg-no-repeat bg-cover bg-center bg-fixed min-h-screen">
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <p className="text-gray-500 text-lg">
+        React and Tailwind CSS in action ya boiiii
+      </p>
+    </div>
     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
       <p className="text-3xl text-gray-700 font-bold mb-5">
         Welcome!
